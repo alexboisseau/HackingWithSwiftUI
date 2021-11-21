76de8cf5b1e513f7edf6ae0e2e5717b0e33a5f78
Alex Boisseau <alex.boisseau79@gmail.com>
2021-11-21 10:30:36 +0100

    🎉 Variables, constants, strings, and numbers

---
+++ Day1.swift
@@ -0,0 +1,23 @@
+// DAY 1 - VARIABLES, CONSTANTS, STRINGS AND NUMBER
+
+// *-- How to create variables and constants --*
+let teamName: String = "Manchester City" // Constant, the value can't change in the futur
+var stadiumName: String = "Etihad Stadium" // Variable, the value can change in the futur
+stadiumName = "Crypto.com Arena" // We don't need to reuse the keyword "var". This keyword is used only for the declaration.
+
+// *-- How to create Strings
+let playerName: String = "Jake Grealish" // On one line
+let teamPlayers: String = """
+Jake Grealish
+Kevin De Bruyne
+Phil Foden
+...
+""" // On several lines
+
+print(teamPlayers)
+
+// *-- How to store whole numbers --*
+let creationDate: Int = 1880 // Int is the type use for whole numbers
+
+// *-- How to store decimale numbers --*
+let matchScore: Double = 4.5 // Double is the type use for decimal numbers
