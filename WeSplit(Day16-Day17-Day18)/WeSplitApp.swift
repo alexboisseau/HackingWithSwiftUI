363ef431100ec93e9041229bf752fc007b5a0040
Alex Boisseau <alex.boisseau79@gmail.com>
2021-12-10 08:45:00 +0100

    :tada: First project with SwiftUI 😎

---
+++ WeSplit(Day16-Day17-Day18)/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Alex Boisseau on 09/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
