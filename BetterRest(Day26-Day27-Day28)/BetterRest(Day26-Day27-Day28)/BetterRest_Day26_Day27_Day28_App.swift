1e2ea7ca5f41d297b759443e56f12e1777e513e0
Alex Boisseau <alex.boisseau79@gmail.com>
2021-12-20 05:36:09 +0100

    :rocket: Starting new project with some machine learning for new three days

---
+++ BetterRest(Day26-Day27-Day28)/BetterRest(Day26-Day27-Day28)/BetterRest_Day26_Day27_Day28_App.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRest_Day26_Day27_Day28_App.swift
+//  BetterRest(Day26-Day27-Day28)
+//
+//  Created by Alex Boisseau on 20/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct BetterRest_Day26_Day27_Day28_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
