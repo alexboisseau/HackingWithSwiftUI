d38be421feacf53bec004f7cb2da046eacf5132d
Alex Boisseau <alex.boisseau79@gmail.com>
2021-12-24 09:59:10 +0100

    Create a .gitignore and initialize the fifth project

---
+++ WordScramble(Day29-Day30-Day31)/WordScramble(Day29-Day30-Day31)/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble(Day29-Day30-Day31)
+//
+//  Created by Alex Boisseau on 24/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WordScramble(Day29-Day30-Day31)/WordScramble(Day29-Day30-Day31)/WordScramble_Day29_Day30_Day31_App.swift
@@ -0,0 +1,17 @@
+//
+//  WordScramble_Day29_Day30_Day31_App.swift
+//  WordScramble(Day29-Day30-Day31)
+//
+//  Created by Alex Boisseau on 24/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct WordScramble_Day29_Day30_Day31_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
